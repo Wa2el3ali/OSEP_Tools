abcce37fe8ad4672dcfddaff7d37f9599bb49581
wa2el <wa2el.3ali@gmail.com>
2026-01-15 13:24:15 +0200

    add script7

--- nmap_auto1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-nmap -Pn -sS -p- -oA $1-all-ports $1 && cat $1-all-ports.nmap | grep ^[0-9] | cut -f1 -d '/' | sed ':a;N;$!ba;s/\n/,/g' | tee $1-open-ports && nmap -sCV -p $(cat $1-open-ports) -oA $1-open-ports-scan $1
-
-
-
-
