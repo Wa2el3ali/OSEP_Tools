e0af3e7d10b548f7618be29122677ae331b2a098
wa2el <wa2el.3ali@gmail.com>
2026-01-15 13:18:38 +0200

    add script5

--- nmap_auto3.sh
+++ nmap_auto3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 timestamp=$(date +%Y%m%d_%H%M%S)
-nmap -Pn -sS -p- -oA $1-all-ports_$timestamp $1 | grep ^[0-9] | cut -f1 -d '/' | sed ':a;N;$!ba;s/\n/,/g' | tee $1-open-ports && nmap -sCV -p $(cat $1-open-ports) -oA $1-open-ports-scan_$timestamp $1
+nmap -Pn -sS -p- -oA $1-all-ports_$timestamp $1 | grep ^[0-9] | cut -f1 -d '/' | sed ':a;N;$!ba;s/\n/,/g' | tee $1-open-ports_$timestamp && nmap -sCV -p $(cat $1-open-ports) -oA $1-open-ports-scan_$timestamp $1
 
 
 
