a4cf9b6b0ee44034d0dfac87983211ece3c9842b
wa2el <wa2el.3ali@gmail.com>
2026-01-28 09:49:55 +0200

    nmap_auto4 max retries

--- nmap_auto4.sh
+++ nmap_auto4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 timestamp=$(date +%Y%m%d-%H%M%S)
-nmap -Pn -sS -p- -oN $1-all-ports_$timestamp.nmap $1 | grep ^[0-9] | cut -f1 -d '/' | sed ':a;N;$!ba;s/\n/,/g' | tee $1-open-ports_$timestamp.txt && nmap -sCV -p $(cat $1-open-ports_$timestamp.txt) -oN $1-open-ports-scan_$timestamp.nmap $1
+nmap -Pn -sS -p- --max-retries 0 -oN $1-all-ports_$timestamp.nmap $1 | grep ^[0-9] | cut -f1 -d '/' | sed ':a;N;$!ba;s/\n/,/g' | tee $1-open-ports_$timestamp.txt && nmap -sCV -p $(cat $1-open-ports_$timestamp.txt) -oN $1-open-ports-scan_$timestamp.nmap $1
 
 
 
